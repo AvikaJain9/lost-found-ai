2290effc94df531c22529f7a5dd7c8fa37360e18
AvikaJain9 <avikajain9@gmail.com>
2026-05-27 08:12:06 -0400

    Update models.py

--- backend/models.py
+++ backend/models.py
@@ -1,1 +1,12 @@
 
+from sqlalchemy import Column, Integer, String
+from database import Base
+
+class Item(Base):
+    __tablename__ = "items"
+
+    id = Column(Integer, primary_key=True, index=True)
+    type = Column(String)  # "lost" or "found"
+    title = Column(String)
+    description = Column(String)
+    location = Column(String)
